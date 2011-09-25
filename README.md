95190c9aeb001ac014fc7e876391aecbd2eb43fc
Moses <nnythm@gmail.com>
2011-09-25 13:25:39 -0700

    added partial documentation, need to update README.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 #Mongfuscate
-Takes sensitive information, stores it, and returns an obfuscated, unique identifier.
+
+##Premise
+Takes sensitive information, stores it, and returns an obfuscated, unique identifier.  
+Inspired by [Stripe](https://github.com/stripe)
+
+###Sample Usage
+```python
+from mongfuscate import Mongfuscate
+from pymongo import Connection
+
+collection = Connection().awesomedb.supercollection
+obfusc = Mongfuscate(collection) #making the mongfuscate object
+
+obf = obfusc.obfuscate("username") #now I have a sweet obfuscated version
+real = obfusc.clarify(obf) #okay, done with playing with the obfuscated version, now I need the real version again
+
+funky_obfusc = Mongfuscate(collection,lambda x : "".join([y for y in x])) #trivial example of passing in a lambda
+obf = funky_obfusc.obfuscate("username") #now I have a sweet obfuscated version
+real = funky_obfusc.clarify(obf) #okay, done with playing with the obfuscated version, now I need the real version again
+
+#note: regardless of the function that is passed in, it should appear the same way to the end-user
+```
--- mongfuscate.py
+++ mongfuscate.py
@@ -1,3 +1,8 @@
+'''
+Obfuscates sensitive data with pymongo
+
+Takes data that is too sensitive to touch for long (Social Security Number, Credit Card Number, Full Name, etc) and obfuscates it to a unique identifier, specified by the user.
+'''
 import string
 from random import Random
 
@@ -7,7 +12,21 @@ DEFAULT_FUNC = lambda word : "".join([rand.choice(RAND_SEARCH_SPACE) for letter 
 REPEAT = 50
 
 class Mongfuscate:
+    """
+    The object you need to instantiate to use Mongfuscate.
+
+    You should only need one per type of obfuscation.  If you want to obfuscate two types of data differently, it would make sense for you to make two instances.
+    """
+
+
     def __init__(self, collection, function=DEFAULT_FUNC, repeat = REPEAT):
+        """
+        Makes an instance of Mongfuscate
+
+        collection should be a pymongo collection
+        function should be a lambda that expects the type of input you pass in and returns a basic, non-deterministic output so that collisions are random instead of deterministic.  The default function takes a string and replaces every character with a random letter or digit, but anything that doesn't use non-standard objects will work.
+        repeat should be an integer, and specifies the number of times it should keep running the function to try to avoid a collision. The default is 50.
+        """
         self.collection = collection
         self.collection.ensure_index([("secret",1),("unsecret",1)],unique=True)
 
@@ -15,7 +34,11 @@ class Mongfuscate:
         self.repeat = repeat
 
     def obfuscate(self, secret):
-        #ensure secret is a string
+        """
+        Obfuscates the secret, and saves the secret, obfuscated pair in the collection
+
+        If secret has not been seen before, and the obfuscated version is uniquely generated successfully, returns the obfuscated version.  If the secret has been seen before, returns the obfuscated version that was previously used.  If the obfuscated version has been seen before, tries to create new obfuscated versions until it successfully creates a unique one.  If it cannot, it returns None.  May throw a pymongo OperationError
+        """
         unsecret = self.__get_unsecret(secret)
 
         attempts = 0;
@@ -34,6 +57,11 @@ class Mongfuscate:
             return None
 
     def clarify(self, unsecret):
+        """
+        Takes the obfuscated version of a secret and returns the raw secret.
+
+        If it does not recognize the obfuscated version, it will return None
+        """
         secret = self.collection.find_one({"unsecret":unsecret},{"secret":1})
         if secret != None and "secret" in secret:
             return secret["secret"]
@@ -41,6 +69,11 @@ class Mongfuscate:
             return None
 
     def forget(self, secret, unsecret):
+        """
+        Forgets the specified secret, obfuscated secret pair.
+
+        If the secret, unsecret pair didn't aready exist, returns False. Otherwise, on success, returns True. May throw a pymongo OperationError
+        """
         if self.collection.remove({"secret":secret,"unsecret":unsecret},safe=True)["n"] > 0:
             return True
         else:
@@ -62,8 +95,12 @@ class Mongfuscate:
             return False
 
     def __get_unsecret(self, secret):
+        """
+        Fetches the obfuscated version of a secret if it has already been put into the collection
+
+        If it does not exist in the collection, returns None
+        """
         temp = self.collection.find_one({"secret":secret},{"unsecret":1})
-#        temp = self.collection.find_one()
         if temp != None and "unsecret" in temp:
             return temp["unsecret"]
         else:
